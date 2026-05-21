636106659047b52fd4c3e6ebfd014c32b928fd57
Nicolas Acevedo <ao.nicolasr@gmail.com>
2026-05-21 16:28:38 -0500

    Initialize repo

---
+++ app/components/layout/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+                &copy; 2024 My Company. All rights reserved.
+            </p>
+        </div>
+    </footer>
+</template>
---
+++ app/components/layout/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar">
+        <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+            <img src="/" alt="Logo">
+        </a>
+        </div>
+    
+        <div class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="/">Home</a>
+            <a class="navbar-item" href="/about">About</a>
+            <a class="navbar-item" href="/contact">Contact</a>
+        </div>
+        </div>
+    </nav>
+</template>
